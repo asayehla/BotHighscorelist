a11dbc402ede505fe8d5e35b3d05872195154c12
asayehla <asayehla@hotmail.com>
2019-08-27 16:49:04 +0200

    added onchange

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,39 +3,56 @@ import search from '../img/search.svg';
 import BotItem from './BotItem';
 
 const Filters = props => {
-    //const [botList, setbotList] = useState([]);
+    //const [categoryFilteredData, setCategoryFilteredData] = useState([]);
+    const [activeFilter, setActiveFilter] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
-
-    const [showFilters, setshowFilter] = useState(true);
-    var botData = props.botData;
+    const [isFiltered, setIsFiltered] = useState(false);
+    const [showFilters, setShowFilter] = useState(true);
 
     //Searchbar
     const [searchValue, setSearchValue] = useState("");
+
     const handleSearchInputChanges = (e) => {
+        let botData = props.botData;
         setSearchValue(e.target.value);
-        //filter
-        return (
+
+        if (e.target.value !== "") {
             setFilteredData(
                 botData.filter((botData) => {
-                    return botData.name.toLowerCase().match(searchValue.toLowerCase())
+                    return botData.name.toLowerCase().match(e.target.value.toLowerCase())
                 })
             )
-        )
+            //set isFiltered for rending filteredlist
+            setIsFiltered(true);
+        } else {
+            setIsFiltered(false);
+        }
+
+
     }
     const resetInputField = () => {
         setSearchValue("");
     }
+
     const callSearchFunctionOnEnter = (e) => {
         e.preventDefault();
-        handleSearchInputChanges();
         resetInputField();
     }
-    //end of searchbar
+    //end of Searchbar
 
+    //Filter Categories
+    const handleCategoryChange = (e) => {
+        if (e.target.checked) {
+            setActiveFilter([...activeFilter, e.target.value])
+        }
 
-    function toogleShowFilter(e) {
-        //setshowFilter=!setshowFilter;
     }
+
+    //toogle ShowFilter
+    const clickhidefilter = (e) => { setShowFilter(false) }
+    const clickshowfilter = (e) => { setShowFilter(true) }
+
+
     const divStyle = { margin: '10px' };
     let content = <p>Loading bots...</p>;
     if (!props.isLoading) {
@@ -54,50 +71,64 @@ const Filters = props => {
                     </form>
                 </div>
 
-                <div className="categorybar"
-                    onClick={toogleShowFilter}>
-                    {/* if true showFilters true */}
+
+                <div className="categorybar ">
                     {showFilters ? (
                         <div>
-                            <div className="filterbar">
-                                <p>Filter by:</p><p>Hide filter{' '}
+                            <div className="filterbar" onClick={clickhidefilter}>
+                                <p>Filter by:{' '}{activeFilter}</p><p>Hide filter{' '}
                                     <input type="checkbox" className="trianglelight" /></p>
                             </div>
                             <div className="categorybar">
                                 <div className="category">
-                                    <input type="checkbox" className="star" style={divStyle} /><label htmlFor="star">Favorites</label>
+                                    <input type="checkbox" className="star" style={divStyle} value="star"
+                                        onChange={handleCategoryChange} />
+                                    <label htmlFor="star">Favorites</label>
                                 </div>
-                                {props.categories.map((c, idx) => <div className="category" key={idx}>
-                                    <input type="checkbox" className="categoryCheck" />
-                                    <label htmlFor="categryCheck">{c}</label></div>)}
+                                {props.categories.map((c, idx) =>
+                                    <div className="category" key={idx} onChange={handleCategoryChange}>
+                                        <input type="checkbox" className="categoryCheck"
+                                            value={c} />
+                                        <label htmlFor="categoryCheck">{c}</label></div>)}
                             </div>
                         </div>
                     ) : (
-                            <div className="filterbar">
-                                <p>Active filter:{' '}{'activeFilter'}</p>
+                            <div className="filterbar" onClick={clickshowfilter}>
+                                <p>Active filter:{' '}{activeFilter}</p>
+                                {/*  {activeFilter.map(c,idx)=><p>{activeFilter[idx]}</p>} */}
                                 <p>Show filters{' '}
                                     <input type="checkbox" className="trianglelight" /></p>
                             </div>
                         )}
                 </div>
 
+
                 <div className="botbar">
-                    <p>Name{' '}
-                        <input type="checkbox" className="triangledark"
-                        //onChange={props.eventSortName} 
-                        /></p>
-                    <p>Score{' '}
-                        <input type="checkbox" className="triangledark"
-                        //onChange={props.eventSortScore}
-                        /></p>
+                    <p
+                    //onChange={handleSortName} 
+                    >Name{' '}
+                        <input type="checkbox" className="triangledark" /></p>
+                    <p
+                    //onChange={handleSortScore}
+                    >Score{' '}
+                        <input type="checkbox" className="triangledark" /></p>
                 </div>
+
                 <div className="BotList">
                     <section>
+                        {isFiltered ?
 
-                        {botData.map((bot, idx) =>
-                            <BotItem key={idx} botData={bot} idx={idx + 1} />
-                        )}
 
+                            /* filteredData.map((bot, idx) =>
+                            <BotItem key={idx} filteredData={bot} idx={idx + 1} />
+                           )   */
+
+                            console.log(filteredData)
+
+                            : props.botData.map((bot, idx) =>
+                                <BotItem key={idx} botData={bot} idx={idx + 1} />
+                            )
+                        }
                     </section>
                 </div>
             </div>
